05c0b266421b162b92ab8c18f9bc038709d58e19
karthi1155 <srikarthi2017@gmail.com>
2021-08-23 21:49:47 +0530

    update

--- 3_Implementation/src/operations.c
+++ 3_Implementation/src/operations.c
@@ -1,33 +1,80 @@
 #include "operations.h"
 #include<math.h>
+/**
+ * @brief calculating the series resistance for three resistance
+ * 
+ * @param r1 
+ * @param r2 
+ * @param r3 
+ * @return float 
+ */
 float total_ser(float r1, float r2,float r3)
 { 
     float b;
     b=r1+r2+r3;
     return b;
 }
+/**
+ * @brief calculating parallel resistance
+ * 
+ * @param r1 
+ * @param r2 
+ * @param r3 
+ * @return float 
+ */
 float total_par(float r1, float r2,float r3)
 {
     float c;
     c=((r1*r2*r3)/(r3*r2+r1*r3+r2*r1));
     return c;
 }
+/**
+ * @brief calculating resistance value
+ * 
+ * @param v 
+ * @param i 
+ * @return float 
+ */
 float  cal_res (float v, float i)
 {
     return(v/i);
 }
+/**
+ * @brief kilo ohms to ohms
+ * 
+ * @param r 
+ * @return double 
+ */
 double unit_conv1(double r )
 {
    return r*pow(10,3);
 }
+/**
+ * @brief ohms to kilo ohms
+ * 
+ * @param r 
+ * @return double 
+ */
 double unit_conv2(double r )
 {
     return r*pow(10,-3);
 }
+/**
+ * @brief mega ohms to ohms
+ * 
+ * @param r 
+ * @return double 
+ */
 double unit_conv3(double r )
 {
     return r*pow(10,6);
 }
+/**
+ * @brief ohm to mega ohms
+ * 
+ * @param r 
+ * @return double 
+ */
 double unit_conv4(double r )
 {
     return r*pow(10,-6);
