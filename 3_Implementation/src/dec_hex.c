1b701e67478dad99b11009b96dfb60603688f3cb
dilipkumar26 <67823998+dilipkumar26@users.noreply.github.com>
2021-08-23 22:10:42 +0530

    Update dec_hex.c

--- 3_Implementation/src/dec_hex.c
+++ 3_Implementation/src/dec_hex.c
@@ -4,66 +4,85 @@
 #include<math.h>
 
 
+/**
+ * @brief Function to compute for the 2 functions 
+ * 
+ */
 struct convert
 {
     char hexa[100];
 }b;
 
+/**
+ * @brief Function to compute Decimal to Hexadecimal
+ * 
+ * @return int returns the decimal value
+ */
 
 int hexadec()
 {
     int i,j=0,sum=0,len;
-    printf("Enter a hexa: ");
+    printf("Enter a hexa: "); // user must give the hexadecimal value
     scanf("%s",&b.hexa);
-    len=strlen(b.hexa);
-    for(i=len-1;i>=0;i--)
+    len=strlen(b.hexa); // checks the length of the input
+    for(i=len-1;i>=0;i--) //iterates over the rows
     {
-        if(b.hexa[i]>='0' && b.hexa[i]<='9')
+        if(b.hexa[i]>='0' && b.hexa[i]<='9') //input value should be in the range of 0 to 9
         {
-            sum+=(b.hexa[i]-48) *pow(16,j);
+            sum+=(b.hexa[i]-48) *pow(16,j); //ASCII value 0 is 48
             j++;
         }
-        else if(b.hexa[i]>='A' && b.hexa[i]<='F')
+        else if(b.hexa[i]>='A' && b.hexa[i]<='F') // input value should be in the range of 0 to 9
         {
-            sum+=(b.hexa[i]-55) *pow(16,j);
+            sum+=(b.hexa[i]-55) *pow(16,j); // ASCII value of A is 55
             j++;
         }
     }
-    printf("Decimal number of %s is %d",b.hexa,sum);
-    return 0;
+    printf("Decimal number of %s is %d",b.hexa,sum); // prints the output for the user input (Decimal value)
+    return 0; 
 }
+
+/**
+ * @brief Function to compute Decimal to Hexadecimal
+ * 
+ * @return int retuns the Hexadecimal value
+ */
 int dechexa()
 {
     long dec,qu,rem;
     int i,j=0;
-    printf("Enter decimal number: ");
+    printf("Enter decimal number: "); // user must give the decimal value
     scanf("%ld", &dec);
     qu=dec;
     while(qu != 0)
     {
-        rem=qu%16;
+        rem=qu%16; // remainder will stores the value of quotient divided by 16
         if (rem < 10)
-            b.hexa[j++] = 48 + rem;
+            b.hexa[j++] = 48 + rem; // If remainder value is less than 10 it should be added by 48
         else
-            b.hexa[j++] = 55 + rem;
+            b.hexa[j++] = 55 + rem; // If remainder value is greater than 10 it should be added by 55
         qu = qu / 16;
     }
-    for (i = j; i >= 0; i--)
-            printf("%c",b.hexa[i]);
+    for (i=j;i>=0;i--) 
+            printf("%c",b.hexa[i]); // prints the output for the user input (Hexadecimla value) 
     return 0;
 }
 
-
+/**
+ * @brief To call the main function
+ * 
+ */
 
 void decimal_hexadecimal_conversion()
 {
     int d;
-    printf("Welcome to Base Conversion");
+    printf("Welcome to Base Conversion"); // after compiling gets as the main heading
     do
     {
-        printf("\nEnter your choice \n1. Dec to Hexa \n2. Hexa to Dec\n3. Exit \n");
+        printf("\nEnter your choice \n1. Dec to Hexa \n2. Hexa to Dec\n3. Exit \n"); // it asks for the users choice
         scanf("%d",&d);
-        switch(d)
+        // code to be executed if the user choose anyone of the below
+        switch(d) 
         {
             case 1: dechexa();
             break;
@@ -72,6 +91,5 @@ void decimal_hexadecimal_conversion()
             case 3: exit(0);
             break;
         }
-    }while(d!=3);
-    
+    }while(d!=3); // output should be given if exit option not clicks
 }
