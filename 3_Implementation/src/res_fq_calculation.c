c9123a685f1b893eebcf8842fef4d866d05bc005
258157 <80401577+GanigaPreethu@users.noreply.github.com>
2021-08-23 21:57:31 +0530

    Update res_fq_calculation.c

--- 3_Implementation/src/res_fq_calculation.c
+++ 3_Implementation/src/res_fq_calculation.c
@@ -5,6 +5,11 @@ double f=0.00, L=0.00,C=0.00;
 
 double fq_calculator(double L,double C);
 
+/**
+ * @brief this function asks the user to enter L and C values
+ * 
+ */
+
 void res_fq_calculation()
 {
     printf("Enter inductance in milliHenrys\n"); 
@@ -15,6 +20,14 @@ void res_fq_calculation()
     printf("Resonant frequency of Tank circuit = %lf kHz \n",f);  
 }
 
+
+/**
+ * @brief this function calculates resonant frequency taking inductor and capacitor values
+ * 
+ * @param L 
+ * @param C 
+ * @return double 
+ */
 double fq_calculator(double L,double C)
 {
     f=1.0/(2*3.142*(sqrt(L*pow(10,-3)*C*pow(10,-12)))); 
