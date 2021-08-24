65d1343571933400e3221955d0d4a86bbdf2d3b7
258157 <80401577+GanigaPreethu@users.noreply.github.com>
2021-08-24 22:32:03 +0530

    Update main.c

--- 3_Implementation/main.c
+++ 3_Implementation/main.c
@@ -1,19 +1,18 @@
-
 #include<stdio.h>
 #include<stdlib.h>
 #include"virtual_lab.h"
 
 int main()
 {
-	int choice;
-	printf("--------------Welcome to the ELETRONICS LAB------------");
+	char choice;
+	printf("\n--------------Welcome to the ELETRONICS LAB------------");
 	printf("\n---Menu---\n");
-	printf("\nList of operations\n1)Electrical Parameters\n 2)Base conversions\n 3)EXIT");
+	first:printf("\nList of operations\n1)Electrical Parameters\n 2)Base conversions\n 3)EXIT");
 	printf("\nEnter your choice:");
-	scanf("%d",&choice);
+	scanf("%c",&choice);
 	switch(choice)
 	{
-		case 1: 
+		case '1': 
 		second: printf("\nAvailable Operations\n");
    		 printf("\n"
             "1. Resistance calculation\n"
@@ -25,35 +24,35 @@ int main()
 			"7.Energy stored in Capacitor and Time constant of given circuit\n"
 			"8. Exit");
 
-			int choice_1;
+			char choice_1;
 			printf("\nEnter your choice:");
-			scanf("%d",&choice_1);
+			scanf("%c",&choice_1);
 
 			switch(choice_1)
 			{
 		
-				case 1:
+				case '1':
 				resistance_calculation();
 				break;
-				case 2:
+				case '2':
 				rms_calculation();
 				break;
-				case 3:
+				case '3':
 				average_calculation();
 				break;
-				case 4:
+				case '4':
 				dc_calculation();
 				break;
-				case 5:
+				case '5':
 				Loop_Kvl_Calculation();
 				break;
-				case 6:
+				case '6':
 				res_fq_calculation();
 				break;
-				case 7:
+				case '7':
 				EnergyandTimeconstant();
 				break;
-				case 8:
+				case '8':
 				 exit(0); 
 			
 			default: printf("INVALID CHOICE!!!!!!\nPlease renter your choice");
@@ -63,7 +62,7 @@ int main()
 		 break;
 
 
-		case 2:
+		case '2':
 				third: printf("\nAvailable Operations\n");
    		 printf("\n1. Binary <-> Gray conversion \n"
             "2. Decimal <-->  Hexadecimal conversion\n"
@@ -71,25 +70,25 @@ int main()
             "4. Decimal <--> Octal conversion\n"
 			"5. Exit");
 
-			int choice_2;
+			char choice_2;
 			printf("\nEnter your choice:");
-			scanf("%d",&choice_2);
+			scanf("%c",&choice_2);
 
 			switch(choice_2)
 			{
-				case 1:
+				case '1':
 				binary_gray_conversion();
 				break;
-				case 2:
+				case '2':
 				decimal_hexadecimal_conversion();
 				break;
-				case 3:
+				case '3':
 				decimal_binary_conversion();
 				break;
-				case 4:
+				case '4':
 				decimal_octal_conversion();
 				break;		
-				case 5: exit(0); 
+				case '5': exit(0); 
 			
 				default: printf("INVALID CHOICE!!!!!!\nPlease renter your choice");
 				goto third;
@@ -97,11 +96,12 @@ int main()
 	        }
 	break;
 
-	case 3:
+	case '3':
 	printf("\nThank you. Exiting the Application\n");
 	exit(0);
 
 	default:printf("INVALID CHOICE!!!!!!\nPlease renter your choice");
+	goto first;
 
     }
 
